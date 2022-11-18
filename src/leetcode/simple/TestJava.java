807a3e09ffb239abb9751f3047727ea2aebe732a
a5365958 <2571706435@qq.com>
2022-11-19 07:59:54 +0800

    二进制求和

--- src/leetcode/simple/TestJava.java
+++ src/leetcode/simple/TestJava.java
@@ -9,6 +9,27 @@ public class TestJava {
         System.out.println(new TestJava().addBinary("1111","1111"));
     }
 
+    /**
+     * 67. 二进制求和
+     * 简单
+     * 919
+     * 相关企业
+     * 给你两个二进制字符串 a 和 b ，以二进制字符串的形式返回它们的和。
+     *
+     *
+     *
+     * 示例 1：
+     *
+     * 输入:a = "11", b = "1"
+     * 输出："100"
+     * 示例 2：
+     *
+     * 输入：a = "1010", b = "1011"
+     * 输出："10101"
+     * @param a
+     * @param b
+     * @return
+     */
     public String addBinary(String a, String b) {
         int i=a.length()-1,j=b.length()-1;
         StringBuilder bitsResult = new StringBuilder();
