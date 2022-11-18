297ddf612fa2e416003f7db30464ae52f4a6bfc8
a5365958 <2571706435@qq.com>
2022-11-18 19:00:47 +0800

    最长公共前缀:字典排序只需考虑第一个和最后一个的最长前缀。

--- src/leetcode/simple/TestJava.java
+++ src/leetcode/simple/TestJava.java
@@ -7,7 +7,15 @@ public class TestJava {
     public static void main(String[] args) throws InterruptedException {
         System.out.println(new TestJava().longestCommonPrefix(new String[]{"cir","car","c"}));
     }
+
+    //编写一个函数来查找字符串数组中的最长公共前缀。
+    //如果不存在公共前缀，返回空字符串 “”。
+    //示例 1：
+    //输入：strs = [“flower”,“flow”,“flight”]
+    //输出：“fl”
+    //最长公共前缀
     public String longestCommonPrefix(String[] strs) {
+        //假设首先理解了字典序概念以后，把数组进行排序，这时候只需要看第一个字符串和最后一个字符串的公共前缀了，相当于夹逼
         TreeSet<String> set = new TreeSet<>();
         for(String str:strs){
             set.add(str);
