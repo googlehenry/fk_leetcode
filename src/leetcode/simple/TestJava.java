f8570aa2e219010da706a48a34374a2aecba0a91
a5365958 <2571706435@qq.com>
2022-11-19 16:50:46 +0800

    205. 同构字符串

--- src/leetcode/simple/TestJava.java
+++ src/leetcode/simple/TestJava.java
@@ -8,87 +8,69 @@ import java.util.stream.Collectors;
 public class TestJava {
 
     public static void main(String[] args) throws InterruptedException {
-        System.out.println(new TestJava().equalFrequency("bac"));
+        System.out.println(new TestJava().isIsomorphic("foo","bar"));
     }
 
     /**
-     * 2423. 删除字符使频率相同
+     * 205. 同构字符串
      * 简单
-     * 15
+     * 540
      * 相关企业
-     * 给你一个下标从 0 开始的字符串 word ，字符串只包含小写英文字母。你需要选择 一个 下标并 删除 下标处的字符，使得 word 中剩余每个字母出现 频率 相同。
+     * 给定两个字符串 s 和 t ，判断它们是否是同构的。
      *
-     * 如果删除一个字母后，word 中剩余所有字母的出现频率都相同，那么返回 true ，否则返回 false 。
+     * 如果 s 中的字符可以按某种映射关系替换得到 t ，那么这两个字符串是同构的。
      *
-     * 注意：
-     *
-     * 字母 x 的 频率 是这个字母在字符串中出现的次数。
-     * 你 必须 恰好删除一个字母，不能一个字母都不删除。
+     * 每个出现的字符都应当映射到另一个字符，同时不改变字符的顺序。不同字符不能映射到同一个字符上，相同字符只能映射到同一个字符上，字符可以映射到自己本身。
      *
      *
-     * 示例 1：
      *
-     * 输入：word = "abcc"
+     * 示例 1:
+     *
+     * 输入：s = "egg", t = "add"
      * 输出：true
-     * 解释：选择下标 3 并删除该字母，word 变成 "abc" 且每个字母出现频率都为 1 。
      * 示例 2：
      *
-     * 输入：word = "aazz"
+     * 输入：s = "foo", t = "bar"
      * 输出：false
-     * 解释：我们必须删除一个字母，所以要么 "a" 的频率变为 1 且 "z" 的频率为 2 ，要么两个字母频率反过来。所以不可能让剩余所有字母出现频率相同。
+     * 示例 3：
      *
-     *
-     * 暴力破解，那就删除一个字母，把剩下的统计一下看看是不是一样的。
+     * 输入：s = "paper", t = "title"
+     * 输出：true
+     * @param s
+     * @param t
+     * @return
      */
-    public boolean equalFrequency(String word) {
-        Map<Character,Integer> counter = new HashMap<>();
-        for(int i=0;i<word.length();i++){
-            counter.put(word.charAt(i), counter.get(word.charAt(i))==null?1:counter.get(word.charAt(i))+1);
+    public boolean isIsomorphic(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
         }
 
-        List<Integer> freqList = new ArrayList<Integer>(counter.values()).stream().sorted().toList();
-        //[1,1,1,2,3]
+        Map<Character,Character> mapB2A = new HashMap<>();
+        Map<Character,Character> mapA2B = new HashMap<>();
+        for(int i=0;i<s.length();i++){
 
-        if(freqList.get(0)==1){
-            //后面必须一致,return true
-            if(freqList.size()>1) {
-                Integer keep = freqList.get(1);
-                boolean match = true;
-                for (int i = 1; i < freqList.size(); i++) {
-                    if(freqList.get(i)!=keep){
-                        match= false;
-                        break;
-                    }
-                }
+            Character a = s.charAt(i);
+            Character b = t.charAt(i);
 
-                if(!match) {
-                    match = true;
-                    Integer keep2 = freqList.get(freqList.size() - 1) - 1;
-                    if (freqList.size() > 1) {
-                        for (int i = freqList.size() - 2; i >= 0; i--) {
-                            if (freqList.get(i) != keep2) {
-                                match= false;
-                                break;
-                            }
-                        }
-                    }
-                }
-
-                return match;
-            }
-            return true;
-        }else {
-            Integer keep = freqList.get(freqList.size()-1)-1;
-            if(freqList.size()>1) {
-                for (int i = freqList.size() - 2; i >= 0; i--) {
-                    if (freqList.get(i) != keep) {
-                        return false;
-                    }
+            if(!mapB2A.containsKey(b)){
+                mapB2A.put(b,a);
+            }else{
+                Character lastMapping = mapB2A.get(b);
+                if(lastMapping!=a){
+                    return false;
                 }
             }
-            return true;
+
+            if(!mapA2B.containsKey(a)){
+                mapA2B.put(a,b);
+            }else{
+                Character lastMapping = mapA2B.get(a);
+                if(lastMapping!=b){
+                    return false;
+                }
+            }
         }
-
+        return true;
     }
 
 }
