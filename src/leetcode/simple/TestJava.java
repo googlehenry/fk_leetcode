60562ea164f52b57801e9f5c7750343fdeee5d06
a5365958 <2571706435@qq.com>
2022-11-19 12:49:13 +0800

    2423. 删除字符使频率相同

--- src/leetcode/simple/TestJava.java
+++ src/leetcode/simple/TestJava.java
@@ -3,58 +3,58 @@ package leetcode.simple;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class TestJava {
 
     public static void main(String[] args) throws InterruptedException {
-        System.out.println(new TestJava().isValid("{[]}()[]"));
+        System.out.println(new TestJava().equalFrequency("aabbcc"));
     }
 
     /**
-     * 20. 有效的括号
+     * 2423. 删除字符使频率相同
      * 简单
-     * 3.6K
+     * 15
      * 相关企业
-     * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+     * 给你一个下标从 0 开始的字符串 word ，字符串只包含小写英文字母。你需要选择 一个 下标并 删除 下标处的字符，使得 word 中剩余每个字母出现 频率 相同。
      *
-     * 有效字符串需满足：
+     * 如果删除一个字母后，word 中剩余所有字母的出现频率都相同，那么返回 true ，否则返回 false 。
      *
-     * 左括号必须用相同类型的右括号闭合。
-     * 左括号必须以正确的顺序闭合。
-     * 每个右括号都有一个对应的相同类型的左括号。
+     * 注意：
+     *
+     * 字母 x 的 频率 是这个字母在字符串中出现的次数。
+     * 你 必须 恰好删除一个字母，不能一个字母都不删除。
      *
      *
      * 示例 1：
      *
-     * 输入：s = "()"
+     * 输入：word = "abcc"
      * 输出：true
+     * 解释：选择下标 3 并删除该字母，word 变成 "abc" 且每个字母出现频率都为 1 。
      * 示例 2：
      *
-     * 输入：s = "()[]{}"
-     * 输出：true
-     * 示例 3：
-     *
-     * 输入：s = "(]"
+     * 输入：word = "aazz"
      * 输出：false
-     * @param s
-     * @return
-     * 改进版，进去的顺序固定，那么关闭的顺序也决定了。减少判断。
+     * 解释：我们必须删除一个字母，所以要么 "a" 的频率变为 1 且 "z" 的频率为 2 ，要么两个字母频率反过来。所以不可能让剩余所有字母出现频率相同。
+     *
+     *
+     * 暴力破解，那就删除一个字母，把剩下的统计一下看看是不是一样的。
      */
-    public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        for(int i=0;i<s.length();i++){
-            switch(s.charAt(i)){
-                case '(': stack.add(')');break;
-                case '{': stack.add('}');break;
-                case '[': stack.add(']');break;
-                default:{
-                    if(stack.isEmpty()||stack.pop()!=s.charAt(i)){
-                        return false;
-                    }
-                }
+    public boolean equalFrequency(String word) {
+        Map<Character,Integer> counter = new HashMap<>();
+        for(int i=0;i<word.length();i++){
+            for(int j=0;j<i;j++){
+                counter.put(word.charAt(j), counter.get(word.charAt(j))==null?1:counter.get(word.charAt(j))+1);
             }
+            for(int j=word.length()-1;j>i;j--){
+                counter.put(word.charAt(j), counter.get(word.charAt(j))==null?1:counter.get(word.charAt(j))+1);
+            }
+            if(counter.values().stream().collect(Collectors.toSet()).size()==1){
+                return true;
+            }
+            counter.clear();
         }
-        return stack.isEmpty();
+        return false;
     }
 
 }
