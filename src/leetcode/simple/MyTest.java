89c3d896278065862e3fb4b8be71e096fee3b0a7
a5365958 <2571706435@qq.com>
2022-11-23 07:59:37 +0800

    归并

--- src/leetcode/simple/MyTest.java
+++ src/leetcode/simple/MyTest.java
@@ -7,39 +7,59 @@ import java.util.Map;
 public class MyTest {
 
     public static void main(String[] args) {
-        new MyTest().letterCombinations("23").forEach((it)->System.out.println(it));
+        int[] arr = new int[]{1,3,2,4,5,3,6,8};
+        mSort(arr,0,arr.length-1);
+        System.out.println(arr);
     }
-    String[] arrs = new String[]{"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-    List<String> res = new ArrayList<>();
 
     /**
-     * 回溯，暴力递归，动态层数的循环。电话号码盘。
-     * @param digits
-     * @return
+     * 归并排序手写
+     * @param arr
+     * @param start
+     * @param end
      */
-    public List<String> letterCombinations(String digits) {
+    public static void mSort(int[] arr, int start, int end){
 
-        StringBuilder stringBuilder = new StringBuilder();
 
-        if(digits.length()==0){
-            return res;
+        if(start>=end){
+            return;
         }
-        processOneText(digits,0,stringBuilder);
 
-        return res;
+        int mid = start+(end-start)/2;
+
+        mSort(arr,start,mid);
+        mSort(arr,mid+1,end);
+
+        merge(arr,start,mid,end);
+
     }
 
-    private void processOneText(String digits,int idx, StringBuilder stringBuilder) {
+    public static void merge(int[] arr, int start, int mid, int end){
+        int[] arrTemp = new int[end-start+1];
 
-        if(idx==digits.length()){
-            res.add(stringBuilder.toString());
-            return ;
+        int i = start;
+        int j = mid+1;
+
+        int k = 0; //any partial arr
+
+        while(i<=mid&&j<=end){
+            if(arr[i]<arr[j]){
+                arrTemp[k++] = arr[i++];
+            }else{
+                arrTemp[k++] = arr[j++];
+            }
         }
-        String text = arrs[digits.charAt(idx)-'0'];
-        for(int i=0;i<text.length();i++){
-            stringBuilder.append(text.charAt(i));
-            processOneText(digits,idx+1,stringBuilder);
-            stringBuilder.deleteCharAt(stringBuilder.length()-1);
+
+        //process not 1-one
+        while(i<=mid){
+            arrTemp[k++] = arr[i++];
+        }
+        while(j<=end){
+            arrTemp[k++] = arr[j++];
+        }
+        //store back
+        for(int x=start;x<=end;x++){
+            arr[x] = arrTemp[x-start];
         }
     }
 
