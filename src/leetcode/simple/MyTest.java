a222f18f12e7cec148cbac6d0268c4c9f1bc1b18
a5365958 <2571706435@qq.com>
2022-11-26 22:13:13 +0800

    547. 省份数量:并查集

--- src/leetcode/simple/MyTest.java
+++ src/leetcode/simple/MyTest.java
@@ -1,66 +1,84 @@
 package leetcode.simple;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class MyTest {
 
     public static void main(String[] args) {
-        int[] arr = new int[]{1,3,2,4,5,3,6,8};
-        mSort(arr,0,arr.length-1);
-        System.out.println(arr);
+        int[][] isConnected = new int[][]{{1,0,0,1},{0,1,1,0},{0,1,1,1},{1,0,1,1}};
+        System.out.println(new MyTest().findCircleNum(isConnected));
     }
 
     /**
-     * 归并排序手写
-     * @param arr
-     * @param start
-     * @param end
+     * 547. 省份数量
+     * 中等
+     * 898
+     * 相关企业
+     * 有 n 个城市，其中一些彼此相连，另一些没有相连。如果城市 a 与城市 b 直接相连，且城市 b 与城市 c 直接相连，那么城市 a 与城市 c 间接相连。
+     *
+     * 省份 是一组直接或间接相连的城市，组内不含其他没有相连的城市。
+     *
+     * 给你一个 n x n 的矩阵 isConnected ，其中 isConnected[i][j] = 1 表示第 i 个城市和第 j 个城市直接相连，而 isConnected[i][j] = 0 表示二者不直接相连。
+     *
+     * 返回矩阵中 省份 的数量。
+     *
+     *
+     *
+     * 示例 1：
+     *
+     *
+     * 输入：isConnected = [[1,1,0],[1,1,0],[0,0,1]]
+     * 输出：2
+     * 示例 2：
+     *
+     *
+     * 输入：isConnected = [[1,0,0],[0,1,0],[0,0,1]]
+     * 输出：3
+     * @param isConnected
+     * @return
      */
-    public static void mSort(int[] arr, int start, int end){
+    public int findCircleNum(int[][] isConnected) {
+        //init
+        int[] roots = new int[isConnected.length];
+        init(roots);
 
-
-        if(start>=end){
-            return;
-        }
-
-        int mid = start+(end-start)/2;
-
-        mSort(arr,start,mid);
-        mSort(arr,mid+1,end);
-
-        merge(arr,start,mid,end);
-
-    }
-
-    public static void merge(int[] arr, int start, int mid, int end){
-        int[] arrTemp = new int[end-start+1];
-
-        int i = start;
-        int j = mid+1;
-
-        int k = 0; //any partial arr
-
-        while(i<=mid&&j<=end){
-            if(arr[i]<arr[j]){
-                arrTemp[k++] = arr[i++];
-            }else{
-                arrTemp[k++] = arr[j++];
+        //union
+        for(int i=0;i<isConnected.length;i++){
+            int[] row = isConnected[i];
+            for(int j=0;j<row.length;j++){
+                if(isConnected[i][j]==1 && i!=j){
+                    union(i,j,roots);
+                }
             }
         }
 
-        //process not 1-one
-        while(i<=mid){
-            arrTemp[k++] = arr[i++];
+        for(int i=0;i<roots.length;i++){
+            roots[i] = find(roots[i],roots);
         }
-        while(j<=end){
-            arrTemp[k++] = arr[j++];
+
+        return (int)Arrays.stream(roots).distinct().count();
+
+    }
+
+    private void init(int[] roots){
+        for(int i=0; i<roots.length; i++){
+            roots[i] = i;
         }
-        //store back
-        for(int x=start;x<=end;x++){
-            arr[x] = arrTemp[x-start];
+    }
+
+    private int find(int x,int[] roots){
+        if(roots[x]==x){
+            return x;
+        }else{
+            roots[x] = find(roots[x], roots);
+            return roots[x];
         }
+    }
+
+    private void union(int x, int y, int[] roots){
+        int xx = find(x,roots);
+        int yy = find(y,roots);
+        roots[xx] = yy;
     }
 
 
