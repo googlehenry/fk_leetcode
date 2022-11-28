47386182a266f05d6052a58cc1f016ca700beeb8
a5365958 <2571706435@qq.com>
2022-11-28 08:49:31 +0800

    200. 岛屿数量

--- src/leetcode/simple/MyTest.java
+++ src/leetcode/simple/MyTest.java
@@ -5,81 +5,77 @@ import java.util.*;
 public class MyTest {
 
     public static void main(String[] args) {
-        int[][] isConnected = new int[][]{{1,0,0,1},{0,1,1,0},{0,1,1,1},{1,0,1,1}};
-        System.out.println(new MyTest().findCircleNum(isConnected));
+        char[][] isConnected = new char[][]{{'1', '1', '0', '0', '0'}, {'1', '1', '0', '0', '0'}, {'0', '0', '1', '0', '0'}, {'0', '0', '0', '1', '1'}};
+        System.out.println(new MyTest().numIslands(isConnected));
     }
 
     /**
-     * 547. 省份数量
+     * 200. 岛屿数量
      * 中等
-     * 898
+     * 2K
      * 相关企业
-     * 有 n 个城市，其中一些彼此相连，另一些没有相连。如果城市 a 与城市 b 直接相连，且城市 b 与城市 c 直接相连，那么城市 a 与城市 c 间接相连。
+     * 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
      *
-     * 省份 是一组直接或间接相连的城市，组内不含其他没有相连的城市。
+     * 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
      *
-     * 给你一个 n x n 的矩阵 isConnected ，其中 isConnected[i][j] = 1 表示第 i 个城市和第 j 个城市直接相连，而 isConnected[i][j] = 0 表示二者不直接相连。
-     *
-     * 返回矩阵中 省份 的数量。
+     * 此外，你可以假设该网格的四条边均被水包围。
      *
      *
      *
      * 示例 1：
      *
-     *
-     * 输入：isConnected = [[1,1,0],[1,1,0],[0,0,1]]
-     * 输出：2
+     * 输入：grid = [
+     *   ["1","1","1","1","0"],
+     *   ["1","1","0","1","0"],
+     *   ["1","1","0","0","0"],
+     *   ["0","0","0","0","0"]
+     * ]
+     * 输出：1
      * 示例 2：
      *
-     *
-     * 输入：isConnected = [[1,0,0],[0,1,0],[0,0,1]]
+     * 输入：grid = [
+     *   ["1","1","0","0","0"],
+     *   ["1","1","0","0","0"],
+     *   ["0","0","1","0","0"],
+     *   ["0","0","0","1","1"]
+     * ]
      * 输出：3
-     * @param isConnected
+     * @param grid
      * @return
+     * DFS深度优先递归遍历
      */
-    public int findCircleNum(int[][] isConnected) {
-        //init
-        int[] roots = new int[isConnected.length];
-        init(roots);
+    public int numIslands(char[][] grid) {
 
-        //union
-        for(int i=0;i<isConnected.length;i++){
-            int[] row = isConnected[i];
-            for(int j=0;j<row.length;j++){
-                if(isConnected[i][j]==1 && i!=j){
-                    union(i,j,roots);
+        int counter = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    counter++;
+                    dfs(grid, i, j);
                 }
             }
         }
-
-        for(int i=0;i<roots.length;i++){
-            roots[i] = find(roots[i],roots);
-        }
-
-        return (int)Arrays.stream(roots).distinct().count();
-
+        return counter;
     }
 
-    private void init(int[] roots){
-        for(int i=0; i<roots.length; i++){
-            roots[i] = i;
-        }
-    }
+    public void dfs(char[][] grid, int x, int y) {
 
-    private int find(int x,int[] roots){
-        if(roots[x]==x){
-            return x;
-        }else{
-            roots[x] = find(roots[x], roots);
-            return roots[x];
+        if (notInArea(grid, x, y)) {
+            return;
+        }
+        if (grid[x][y] == '0') {
+            return;
+        }
+        if (grid[x][y] == '1') {
+            grid[x][y] = '2';
+            dfs(grid, x, y - 1);
+            dfs(grid, x, y + 1);
+            dfs(grid, x - 1, y);
+            dfs(grid, x + 1, y);
         }
     }
 
-    private void union(int x, int y, int[] roots){
-        int xx = find(x,roots);
-        int yy = find(y,roots);
-        roots[xx] = yy;
+    public boolean notInArea(char[][] grid, int x, int y) {
+        return x >= grid.length || y >= grid[0].length || x < 0 || y < 0;
     }
-
-
 }
