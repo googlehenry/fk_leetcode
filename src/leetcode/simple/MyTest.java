77d6e9be36ee340afcaa72d6825a6fad8f407cbf
a5365958 <2571706435@qq.com>
2022-11-29 19:14:59 +0800

    6. Z 字形变换

--- src/leetcode/simple/MyTest.java
+++ src/leetcode/simple/MyTest.java
@@ -5,67 +5,83 @@ import java.util.*;
 public class MyTest {
 
     public static void main(String[] args) {
-        System.out.println(new MyTest().findMedianSortedArrays(new int[]{1,3},new int[]{2}));
+        System.out.println(new MyTest().convert("AB",1));
     }
 
     /**
-     * 4. 寻找两个正序数组的中位数
-     * 困难
-     * 6.1K
+     * 6. Z 字形变换
+     * 中等
+     * 1.9K
      * 相关企业
-     * 给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的 中位数 。
+     * 将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。
      *
-     * 算法的时间复杂度应该为 O(log (m+n)) 。
+     * 比如输入字符串为 "PAYPALISHIRING" 行数为 3 时，排列如下：
      *
+     * P   A   H   N
+     * A P L S I I G
+     * Y   I   R
+     * 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："PAHNAPLSIIGYIR"。
+     *
+     * 请你实现这个将字符串进行指定行数变换的函数：
+     *
+     * string convert(string s, int numRows);
      *
      *
      * 示例 1：
      *
-     * 输入：nums1 = [1,3], nums2 = [2]
-     * 输出：2.00000
-     * 解释：合并数组 = [1,2,3] ，中位数 2
+     * 输入：s = "PAYPALISHIRING", numRows = 3
+     * 输出："PAHNAPLSIIGYIR"
      * 示例 2：
+     * 输入：s = "PAYPALISHIRING", numRows = 4
+     * 输出："PINALSIGYAHRPI"
+     * 解释：
+     * P     I    N
+     * A   L S  I G
+     * Y A   H R
+     * P     I
+     * 示例 3：
      *
-     * 输入：nums1 = [1,2], nums2 = [3,4]
-     * 输出：2.50000
-     * 解释：合并数组 = [1,2,3,4] ，中位数 (2 + 3) / 2 = 2.5
-     * @param nums1
-     * @param nums2
+     * 输入：s = "A", numRows = 1
+     * 输出："A"
+     * @param s
+     * @param numRows
      * @return
-     * 忽略复杂度：合并数组再找中位数，复杂度(M+N)
+     *
+     * 利用二位表暴力排布
      */
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        //归并
-        int[] merge = new int[nums1.length+nums2.length];
+    public String convert(String s, int numRows) {
+        Character[][] arrs = new Character[numRows][s.length()];
+        int row = 0;
+        int col = 0;
+        int step = 1;
+        int idx = 0;
+        while(idx<s.length()){
+            arrs[row][col] = s.charAt(idx);
 
-        int earlyEndIdx = merge.length/2+1;//odd and even
-        int i = 0;
-        int j = 0;
-        int m = 0;
-        while(i<nums1.length || j<nums2.length){
-            if(i<nums1.length&&j<nums2.length){
-                if(nums1[i]<=nums2[j]){
-                    merge[m++] = nums1[i++];
-                }else{
-                    merge[m++] = nums2[j++];
-                }
-            }else if(i<nums1.length){
-                merge[m++] = nums1[i++];
-            }else if(j<nums2.length){
-                merge[m++] = nums2[j++];
+            if(numRows==1){
+                step = 0;
+                col++;
             }
+            else if(row==numRows-1){
+                step = -1;
+                col++;
+            }else if(row==0){
+                step = 1;
+                col++;
+            }
+            row += step;
+            idx++;
+        }
 
-            //(m+n)/2,how to make it logn?
-            if(m>earlyEndIdx){
-                break;
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int i=0;i<numRows;i++){
+            for(int j=0;j<s.length();j++){
+                if(arrs[i][j]!=null) stringBuilder.append(arrs[i][j]);
             }
         }
 
-        if(merge.length%2==0){
-            return (merge[merge.length/2-1]+merge[merge.length/2])/2.0;
-        }else{
-            return merge[merge.length/2];
-        }
+        return stringBuilder.toString();
+
     }
 
 }
