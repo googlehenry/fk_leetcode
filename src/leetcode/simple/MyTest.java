94c59bb7872ae5395a4548200a00ef522a1bc898
a5365958 <2571706435@qq.com>
2022-11-29 16:46:53 +0800

    43.大数相乘

--- src/leetcode/simple/MyTest.java
+++ src/leetcode/simple/MyTest.java
@@ -27,83 +27,66 @@ public class MyTest {
      *
      * 输入: num1 = "123", num2 = "456"
      * 输出: "56088"
+     * 123*11=123*1+123*10
      * @param num1
      * @param num2
      * @return
      */
     public String multiply(String num1, String num2) {
-        //123
-        //456
-        //----
-        //..8...1
-        //.0+1...0
+       if(num1.equals("0")||num2.equals("0")) return "0";
 
-        if(num1.equals("0")||num2.equals("0"))return "0";
 
-        //123*456=123*6+123*500+123*400
-        int i = num1.length()-1;
-
-        StringBuilder prevValue = new StringBuilder();
-        while(i>=0){
-            int n = num1.charAt(i)-'0';
-            StringBuilder stepValue = new StringBuilder();
-            for(int k=num1.length()-1;k>i;k--){
-                stepValue.append('0');
+       StringBuilder result = new StringBuilder();
+        for(int i=num1.length()-1;i>=0;i--){
+            StringBuilder stringBuilder = new StringBuilder();
+            for(int j=num1.length()-1;j>i;j--){
+                stringBuilder.append("0");
             }
+            int n = num1.charAt(i)-'0';
             int carry = 0;
-            int total = 0;
             for(int j=num2.length()-1;j>=0;j--){
                 int m = num2.charAt(j)-'0';
-                total = m*n+carry;
-                carry = total / 10;
+                int total = m*n+carry;
+                carry = total/10;
                 total = total % 10;
-
-                stepValue.append(total);
+                stringBuilder.append(total);
             }
             if(carry>0){
-                stepValue.append(carry);
+                stringBuilder.append(carry);
             }
 
-            //add with prev step
-            prevValue = addUp(prevValue,stepValue.reverse());
-
-            i--;
+            result = addUp(result,stringBuilder.reverse());
         }
 
-
-        return prevValue.toString();
+        return result.toString();
     }
 
-    private StringBuilder addUp(StringBuilder prevSum,StringBuilder stepValue){
-        if(prevSum.isEmpty())return stepValue;
-
-        String a = prevSum.toString();
-        String b = stepValue.toString();
-
-        int i = a.length()-1,j=b.length()-1;
-        int sum = 0;
+    private StringBuilder addUp(StringBuilder prev,StringBuilder val){
+        String a = prev.toString();
+        String b = val.toString();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
         int carry = 0;
-        StringBuilder result = new StringBuilder();
+        StringBuilder stringBuilder = new StringBuilder();
         while(i>=0||j>=0){
-            sum +=carry;
+            int sum = carry;
             if(i>=0){
-                sum+=a.charAt(i)-'0';
+                sum += a.charAt(i)-'0';
                 i--;
             }
             if(j>=0){
-                sum+=b.charAt(j)-'0';
+                sum += b.charAt(j)-'0';
                 j--;
             }
             carry = sum/10;
-            sum = sum%10;
-
-            result.append(sum);
-
-            sum = 0;
+            sum = sum %10;
+            stringBuilder.append(sum);
         }
         if(carry>0){
-            result.append(carry);
+            stringBuilder.append(carry);
         }
-        return result.reverse();
+        return stringBuilder.reverse();
     }
+
+
 }
