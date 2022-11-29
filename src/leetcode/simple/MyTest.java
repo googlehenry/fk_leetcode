9546e217352037b06d51908d112c0aa384c227c6
a5365958 <2571706435@qq.com>
2022-11-29 16:24:15 +0800

    43.大数相乘

--- src/leetcode/simple/MyTest.java
+++ src/leetcode/simple/MyTest.java
@@ -5,77 +5,105 @@ import java.util.*;
 public class MyTest {
 
     public static void main(String[] args) {
-        char[][] isConnected = new char[][]{{'1', '1', '0', '0', '0'}, {'1', '1', '0', '0', '0'}, {'0', '0', '1', '0', '0'}, {'0', '0', '0', '1', '1'}};
-        System.out.println(new MyTest().numIslands(isConnected));
+        System.out.println(new MyTest().multiply("123","456"));
     }
 
     /**
-     * 200. 岛屿数量
+     * 43. 字符串相乘
      * 中等
-     * 2K
+     * 1.1K
      * 相关企业
-     * 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+     * 给定两个以字符串形式表示的非负整数 num1 和 num2，返回 num1 和 num2 的乘积，它们的乘积也表示为字符串形式。
      *
-     * 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
-     *
-     * 此外，你可以假设该网格的四条边均被水包围。
+     * 注意：不能使用任何内置的 BigInteger 库或直接将输入转换为整数。
      *
      *
      *
-     * 示例 1：
+     * 示例 1:
      *
-     * 输入：grid = [
-     *   ["1","1","1","1","0"],
-     *   ["1","1","0","1","0"],
-     *   ["1","1","0","0","0"],
-     *   ["0","0","0","0","0"]
-     * ]
-     * 输出：1
-     * 示例 2：
+     * 输入: num1 = "2", num2 = "3"
+     * 输出: "6"
+     * 示例 2:
      *
-     * 输入：grid = [
-     *   ["1","1","0","0","0"],
-     *   ["1","1","0","0","0"],
-     *   ["0","0","1","0","0"],
-     *   ["0","0","0","1","1"]
-     * ]
-     * 输出：3
-     * @param grid
+     * 输入: num1 = "123", num2 = "456"
+     * 输出: "56088"
+     * @param num1
+     * @param num2
      * @return
-     * DFS深度优先递归遍历
      */
-    public int numIslands(char[][] grid) {
+    public String multiply(String num1, String num2) {
+        //123
+        //456
+        //----
+        //..8...1
+        //.0+1...0
 
-        int counter = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == '1') {
-                    counter++;
-                    dfs(grid, i, j);
-                }
+        if(num1.equals("0")||num2.equals("0"))return "0";
+
+        //123*456=123*6+123*500+123*400
+        int i = num1.length()-1;
+
+        StringBuilder prevValue = new StringBuilder();
+        while(i>=0){
+            int n = num1.charAt(i)-'0';
+            StringBuilder stepValue = new StringBuilder();
+            for(int k=num1.length()-1;k>i;k--){
+                stepValue.append('0');
             }
+            int carry = 0;
+            int total = 0;
+            for(int j=num2.length()-1;j>=0;j--){
+                int m = num2.charAt(j)-'0';
+                total = m*n+carry;
+                carry = total / 10;
+                total = total % 10;
+
+                stepValue.append(total);
+            }
+            if(carry>0){
+                stepValue.append(carry);
+            }
+
+            //add with prev step
+            prevValue = addUp(prevValue,stepValue.reverse());
+
+            i--;
         }
-        return counter;
+
+
+        return prevValue.toString();
     }
 
-    public void dfs(char[][] grid, int x, int y) {
+    private StringBuilder addUp(StringBuilder prevSum,StringBuilder stepValue){
+        if(prevSum.isEmpty())return stepValue;
 
-        if (notInArea(grid, x, y)) {
-            return;
-        }
-        if (grid[x][y] == '0') {
-            return;
-        }
-        if (grid[x][y] == '1') {
-            grid[x][y] = '2';
-            dfs(grid, x, y - 1);
-            dfs(grid, x, y + 1);
-            dfs(grid, x - 1, y);
-            dfs(grid, x + 1, y);
-        }
-    }
+        String a = prevSum.toString();
+        String b = stepValue.toString();
 
-    public boolean notInArea(char[][] grid, int x, int y) {
-        return x >= grid.length || y >= grid[0].length || x < 0 || y < 0;
+        int i = a.length()-1,j=b.length()-1;
+        int sum = 0;
+        int carry = 0;
+        StringBuilder result = new StringBuilder();
+        while(i>=0||j>=0){
+            sum +=carry;
+            if(i>=0){
+                sum+=a.charAt(i)-'0';
+                i--;
+            }
+            if(j>=0){
+                sum+=b.charAt(j)-'0';
+                j--;
+            }
+            carry = sum/10;
+            sum = sum%10;
+
+            result.append(sum);
+
+            sum = 0;
+        }
+        if(carry>0){
+            result.append(carry);
+        }
+        return result.reverse();
     }
 }
