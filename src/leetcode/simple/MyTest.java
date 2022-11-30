914c2fb13d9b662e7654279ee8adf1b9a2d342c4
a5365958 <2571706435@qq.com>
2022-11-30 09:08:11 +0800

    8. 字符串转换整数 (atoi)

--- src/leetcode/simple/MyTest.java
+++ src/leetcode/simple/MyTest.java
@@ -6,48 +6,100 @@ import java.util.*;
 public class MyTest {
 
     public static void main(String[] args) {
-        System.out.println(new MyTest().isPalindrome(1234321));
+        System.out.println(new MyTest().myAtoi("-2147483648"));
     }
 
     /**
-     * 9. 回文数
-     * 简单
-     * 2.3K
+     * 8. 字符串转换整数 (atoi)
+     * 中等
+     * 1.6K
      * 相关企业
-     * 给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
-     *
-     * 回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
-     *
-     * 例如，121 是回文，而 123 不是。
-     *
-     *
+     * 请你来实现一个 myAtoi(string s) 函数，使其能将字符串转换成一个 32 位有符号整数（类似 C/C++ 中的 atoi 函数）。
+     * <p>
+     * 函数 myAtoi(string s) 的算法如下：
+     * <p>
+     * 读入字符串并丢弃无用的前导空格
+     * 检查下一个字符（假设还未到字符末尾）为正还是负号，读取该字符（如果有）。 确定最终结果是负数还是正数。 如果两者都不存在，则假定结果为正。
+     * 读入下一个字符，直到到达下一个非数字字符或到达输入的结尾。字符串的其余部分将被忽略。
+     * 将前面步骤读入的这些数字转换为整数（即，"123" -> 123， "0032" -> 32）。如果没有读入数字，则整数为 0 。必要时更改符号（从步骤 2 开始）。
+     * 如果整数数超过 32 位有符号整数范围 [−231,  231 − 1] ，需要截断这个整数，使其保持在这个范围内。具体来说，小于 −231 的整数应该被固定为 −231 ，大于 231 − 1 的整数应该被固定为 231 − 1 。
+     * 返回整数作为最终结果。
+     * 注意：
+     * <p>
+     * 本题中的空白字符只包括空格字符 ' ' 。
+     * 除前导空格或数字后的其余字符串外，请勿忽略 任何其他字符。
+     * <p>
+     * <p>
      * 示例 1：
-     *
-     * 输入：x = 121
-     * 输出：true
+     * <p>
+     * 输入：s = "42"
+     * 输出：42
+     * 解释：加粗的字符串为已经读入的字符，插入符号是当前读取的字符。
+     * 第 1 步："42"（当前没有读入字符，因为没有前导空格）
+     * ^
+     * 第 2 步："42"（当前没有读入字符，因为这里不存在 '-' 或者 '+'）
+     * ^
+     * 第 3 步："42"（读入 "42"）
+     * ^
+     * 解析得到整数 42 。
+     * 由于 "42" 在范围 [-231, 231 - 1] 内，最终结果为 42 。
      * 示例 2：
-     *
-     * 输入：x = -121
-     * 输出：false
-     * 解释：从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+     * <p>
+     * 输入：s = "   -42"
+     * 输出：-42
+     * 解释：
+     * 第 1 步："   -42"（读入前导空格，但忽视掉）
+     * ^
+     * 第 2 步："   -42"（读入 '-' 字符，所以结果应该是负数）
+     * ^
+     * 第 3 步："   -42"（读入 "42"）
+     * ^
+     * 解析得到整数 -42 。
+     * 由于 "-42" 在范围 [-231, 231 - 1] 内，最终结果为 -42 。
      * 示例 3：
+     * <p>
+     * 输入：s = "4193 with words"
+     * 输出：4193
+     * 解释：
+     * 第 1 步："4193 with words"（当前没有读入字符，因为没有前导空格）
+     * ^
+     * 第 2 步："4193 with words"（当前没有读入字符，因为这里不存在 '-' 或者 '+'）
+     * ^
+     * 第 3 步："4193 with words"（读入 "4193"；由于下一个字符不是一个数字，所以读入停止）
+     * ^
+     * 解析得到整数 4193 。
+     * 由于 "4193" 在范围 [-231, 231 - 1] 内，最终结果为 4193 。
      *
-     * 输入：x = 10
-     * 输出：false
-     * 解释：从右向左读, 为 01 。因此它不是一个回文数。
-     * @param x
-     * @return
-     * 字符串解决+数字反序
+     * 一样的数字反转，先过滤，和注意符号对边界的影响。
      */
-    public boolean isPalindrome(int x) {
-        if(x<0)return false;
-        int old = x;
-        int temp = 0;
-        while(x!=0){
-            temp = temp*10 + x%10;
-            x = x/10;
+    public int myAtoi(String s) {
+        int total = 0;
+        s = s.trim();
+        if (s.length() == 0) return 0;
+
+        int sign = s.charAt(0) == '-' ? -1 : 1;
+        int startIdx = (s.charAt(0)=='+'||s.charAt(0)=='-') ? 1 : 0;
+        for (int i = startIdx; i < s.length(); i++) {
+            Character character = s.charAt(i);
+            if (character >= '0' && character <= '9') {
+                int digit = character - '0';
+                if (sign==1 &&(total > Integer.MAX_VALUE / 10 || (total == Integer.MAX_VALUE / 10 && digit > Integer.MAX_VALUE % 10))) {
+                    total = Integer.MAX_VALUE;
+                    sign = 1;
+                    break;
+                }
+                if(sign==-1 && (-total < Integer.MIN_VALUE / 10 || (-total == Integer.MIN_VALUE / 10 && -digit < Integer.MIN_VALUE % 10))){
+                    total = Integer.MIN_VALUE;
+                    sign = 1;
+                    break;
+                }
+
+                total = total * 10 + digit;
+            }else{
+                break;
+            }
         }
-        return temp == old;
+        return  sign*total;
     }
 
 }
