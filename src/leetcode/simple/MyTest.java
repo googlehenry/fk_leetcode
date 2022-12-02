aa404d7d1aef9e4625b01a6b7555091a7e235bca
a5365958 <2571706435@qq.com>
2022-12-02 12:27:31 +0800

    451. 根据字符出现频率排序

--- src/leetcode/simple/MyTest.java
+++ src/leetcode/simple/MyTest.java
@@ -11,71 +11,68 @@ import java.util.jar.JarEntry;
 public class MyTest {
 
     public static void main(String[] args) {
-        System.out.println(new MyTest().topKFrequent(new int[]{1,1,1,2,2,3},2));
+        System.out.println(new MyTest().frequencySort("raaeaedere"));
     }
 
     /**
-     * 347. 前 K 个高频元素
+     * 451. 根据字符出现频率排序
      * 中等
-     * 1.4K
+     * 445
      * 相关企业
-     * 给你一个整数数组 nums 和一个整数 k ，请你返回其中出现频率前 k 高的元素。你可以按 任意顺序 返回答案。
+     * 给定一个字符串 s ，根据字符出现的 频率 对其进行 降序排序 。一个字符出现的 频率 是它出现在字符串中的次数。
+     *
+     * 返回 已排序的字符串 。如果有多个答案，返回其中任何一个。
      *
      *
      *
      * 示例 1:
      *
-     * 输入: nums = [1,1,1,2,2,3], k = 2
-     * 输出: [1,2]
+     * 输入: s = "tree"
+     * 输出: "eert"
+     * 解释: 'e'出现两次，'r'和't'都只出现一次。
+     * 因此'e'必须出现在'r'和't'之前。此外，"eetr"也是一个有效的答案。
      * 示例 2:
      *
-     * 输入: nums = [1], k = 1
-     * 输出: [1]
-     *
-     *
-     * 提示：
-     *
-     * 1 <= nums.length <= 105
-     * k 的取值范围是 [1, 数组中不相同的元素的个数]
-     * 题目数据保证答案唯一，换句话说，数组中前 k 个高频元素的集合是唯一的
-     *
-     *
-     * 进阶：你所设计算法的时间复杂度 必须 优于 O(n log n) ，其中 n 是数组大小。
-     * @param nums
-     * @param k
-     * @return
-     * 使用PriorityQueue
+     * 输入: s = "cccaaa"
+     * 输出: "cccaaa"
+     * 解释: 'c'和'a'都出现三次。此外，"aaaccc"也是有效的答案。
+     * 注意"cacaca"是不正确的，因为相同的字母必须放在一起。
      */
-    public int[] topKFrequent(int[] nums, int k) {
+    public String frequencySort(String s) {
+        if(s==null || s.length()==1) return s;
 
-        Arrays.sort(nums);
-        int L = 0;
-        int R = 0;
-        PriorityQueue<Map.Entry<Integer,Integer>> topK = new PriorityQueue<>(k,new Comparator<Map.Entry<Integer, Integer>>() {
+        char[] arrs = s.toCharArray();
+        Arrays.sort(arrs);
+
+        PriorityQueue<Map.Entry<Integer,Character>> freqChars = new PriorityQueue<>(s.length(), new Comparator<Map.Entry<Integer, Character>>() {
             @Override
-            public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
+            public int compare(Map.Entry<Integer, Character> o1, Map.Entry<Integer, Character> o2) {
                 return o2.getKey()-o1.getKey();
             }
         });
 
-        while(R<=nums.length){
-            if(R!=nums.length && nums[L]==nums[R]){
+        int L = 0;
+        int R = 1;
+        while(R<arrs.length){
+            if(arrs[L]==arrs[R]){
                 R++;
             }else{
-                int freq = R-L; //aab
-                topK.add(Map.entry(freq,nums[L]));
+                freqChars.add(Map.entry(R-L,arrs[L]));
                 L = R;
                 R++;
             }
         }
+        freqChars.add(Map.entry(R-L,arrs[L]));
 
-        int[] result = new int[k];
-        int counter = 0;
-        while(!topK.isEmpty() && counter<k){
-            result[counter++] = topK.poll().getValue();
+        StringBuilder stringBuilder = new StringBuilder();
+        while(!freqChars.isEmpty()){
+            Map.Entry<Integer,Character> entry = freqChars.poll();
+            for(int i=0;i<entry.getKey();i++){
+                stringBuilder.append(entry.getValue());
+            }
         }
 
-        return result;
+        return stringBuilder.toString();
     }
 
     public static class ListNode {
