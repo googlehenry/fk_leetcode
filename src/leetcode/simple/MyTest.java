393bce4f43a994a1809dfcee3ea0d8144dd73d4e
a5365958 <2571706435@qq.com>
2022-12-02 18:37:04 +0800

    27. 移除元素

--- src/leetcode/simple/MyTest.java
+++ src/leetcode/simple/MyTest.java
@@ -11,54 +11,68 @@ import java.util.jar.JarEntry;
 public class MyTest {
 
     public static void main(String[] args) {
-        System.out.println(new MyTest().findKthLargest(new int[]{3,2,1,5,6,4},2));
+        System.out.println(new MyTest().removeElement(new int[]{3,2,2,3},3));
     }
 
+
     /**
-     215. 数组中的第K个最大元素
-     中等
-     2K
-     相关企业
-     给定整数数组 nums 和整数 k，请返回数组中第 k 个最大的元素。
-
-     请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
-
-     你必须设计并实现时间复杂度为 O(n) 的算法解决此问题。
-
-
-
-     示例 1:
-
-     输入: [3,2,1,5,6,4], k = 2
-     输出: 5
-     示例 2:
-
-     输入: [3,2,3,1,2,4,5,5,6], k = 4
-     输出: 4
-     //建立一个定长数组。应该去建立一个大顶堆，但要固定长度。
+     * 27. 移除元素
+     * 简单
+     * 1.6K
+     * 相关企业
+     * 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+     *
+     * 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+     *
+     * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+     *
+     *
+     *
+     * 说明:
+     *
+     * 为什么返回数值是整数，但输出的答案是数组呢?
+     *
+     * 请注意，输入数组是以「引用」方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
+     *
+     * 你可以想象内部操作如下:
+     *
+     * // nums 是以“引用”方式传递的。也就是说，不对实参作任何拷贝
+     * int len = removeElement(nums, val);
+     *
+     * // 在函数里修改输入数组对于调用者是可见的。
+     * // 根据你的函数返回的长度, 它会打印出数组中 该长度范围内 的所有元素。
+     * for (int i = 0; i < len; i++) {
+     *     print(nums[i]);
+     * }
+     *
+     *
+     * 示例 1：
+     *
+     * 输入：nums = [3,2,2,3], val = 3
+     * 输出：2, nums = [2,2]
+     * 解释：函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。你不需要考虑数组中超出新长度后面的元素。例如，函数返回的新长度为 2 ，而 nums = [2,2,3,3] 或 nums = [2,2,0,0]，也会被视作正确答案。
+     * 示例 2：
+     *
+     * 输入：nums = [0,1,2,2,3,0,4,2], val = 2
+     * 输出：5, nums = [0,1,4,0,3]
+     * 解释：函数应该返回新的长度 5, 并且 nums 中的前五个元素为 0, 1, 3, 0, 4。注意这五个元素可为任意顺序。你不需要考虑数组中超出新长度后面的元素。
+     * @param nums
+     * @param val
+     * @return
      */
-    public int findKthLargest(int[] nums, int k) {
-        int[] resultK = new int[k];
-        for(int i = 0;i<k;i++){
-            resultK[i] = Integer.MIN_VALUE;
+    public int removeElement(int[] nums, int val) {
+        int count = 0;
+        for(int i=0;i<nums.length-count;i++){
+            if(val==nums[i]){
+                //move array
+                for(int j=i;j<nums.length-1;j++){
+                    nums[j] = nums[j+1];
+                }
+                i--;
+                count++;
+            }
         }
-
-        for(int i=0;i<nums.length;i++){
-            int findIdx = resultK.length;
-           for(int j=resultK.length-1;j>=0;j--){
-               if(nums[i]>resultK[j]){
-                  findIdx = j;
-               }
-           }
-
-           for(int j = resultK.length-1; j>findIdx;j--){
-               resultK[j] = resultK[j-1];
-           }
-           if(findIdx<resultK.length) {
-               resultK[findIdx] = nums[i];
-           }
-        }
-        return resultK[resultK.length-1];
+        return nums.length - count;
     }
 
     public static class ListNode {
