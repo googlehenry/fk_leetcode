211b15881bd65f07a32b3f990191f04703bc7a06
a5365958 <2571706435@qq.com>
2022-12-02 11:42:01 +0800

    347. 前 K 个高频元素

--- src/leetcode/simple/MyTest.java
+++ src/leetcode/simple/MyTest.java
@@ -1,5 +1,7 @@
 package leetcode.simple;
 
+import com.sun.source.tree.Tree;
+
 import java.awt.geom.Area;
 import java.lang.reflect.Array;
 import java.util.*;
@@ -8,28 +10,77 @@ import java.util.jar.JarEntry;
 public class MyTest {
 
     public static void main(String[] args) {
-        ListNode rs = new MyTest().reverseList(new ListNode(1,new ListNode(2,new ListNode(3,new ListNode(4,null)))));
-        System.out.println(rs);
+        System.out.println(new MyTest().topKFrequent(new int[]{1,1,1,2,2,3},2));
     }
 
     /**
-        //链表逆序
+     * 347. 前 K 个高频元素
+     * 中等
+     * 1.4K
+     * 相关企业
+     * 给你一个整数数组 nums 和一个整数 k ，请你返回其中出现频率前 k 高的元素。你可以按 任意顺序 返回答案。
+     *
+     *
+     *
+     * 示例 1:
+     *
+     * 输入: nums = [1,1,1,2,2,3], k = 2
+     * 输出: [1,2]
+     * 示例 2:
+     *
+     * 输入: nums = [1], k = 1
+     * 输出: [1]
+     *
+     *
+     * 提示：
+     *
+     * 1 <= nums.length <= 105
+     * k 的取值范围是 [1, 数组中不相同的元素的个数]
+     * 题目数据保证答案唯一，换句话说，数组中前 k 个高频元素的集合是唯一的
+     *
+     *
+     * 进阶：你所设计算法的时间复杂度 必须 优于 O(n log n) ，其中 n 是数组大小。
+     * @param nums
+     * @param k
+     * @return
      */
-    public ListNode reverseList(ListNode head) {
-        if(head==null) return null;
-        if(head.next==null) return head;
-        //1->2->3->4
-        //2->1->3->4
-        ListNode tobeSwapped = head.next;
-        head.next = null;
-        while(tobeSwapped!=null){
-            ListNode remainingHead = tobeSwapped.next;
-            tobeSwapped.next = head;
-            head = tobeSwapped;
-            tobeSwapped = remainingHead;
+    public int[] topKFrequent(int[] nums, int k) {
+
+        Arrays.sort(nums);
+        int L = 0;
+        int R = 0;
+        TreeMap<Integer,List<Integer>> freqToNumberMap = new TreeMap<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2-o1;
+            }
+        });
+
+        while(R<=nums.length){
+            if(R!=nums.length && nums[L]==nums[R]){
+                R++;
+            }else{
+                int freq = R-L; //aab
+                List<Integer> arrs = freqToNumberMap.get(freq)==null?new ArrayList<>() : freqToNumberMap.get(freq);
+                arrs.add(nums[L]);
+                freqToNumberMap.put(freq,arrs);
+                L = R;
+                R++;
+            }
         }
 
-        return head;
+        int[] result = new int[k];
+
+        Iterator<Map.Entry<Integer,List<Integer>>> iterator = freqToNumberMap.entrySet().iterator();
+        int counter = 0;
+        while(iterator.hasNext() && counter<k){
+            List<Integer> arrs = iterator.next().getValue();
+            for(int i=0;i<arrs.size();i++){
+                result[counter++] = arrs.get(i);
+                if(counter>=k)break;
+            }
+        }
+        return result;
     }
 
     public static class ListNode {
