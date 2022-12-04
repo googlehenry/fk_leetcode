7e1e9ef2293897142b76f3ee57ae359c00f8f5f8
a5365958 <2571706435@qq.com>
2022-12-04 12:35:32 +0800

    HashSet 对象hashcode变化导致内存泄露-不要手动override hashcode/equals就可以的啊，要注意。

--- src/leetcode/simple/MyTest2.java
+++ src/leetcode/simple/MyTest2.java
@@ -51,38 +51,6 @@ class Person {
         this.age = age;
     }
     @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + age;
-        result = prime * result + ((password == null) ? 0 : password.hashCode());
-        result = prime * result + ((username == null) ? 0 : username.hashCode());
-        return result;
-    }
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Person other = (Person) obj;
-        if (age != other.age)
-            return false;
-        if (password == null) {
-            if (other.password != null)
-                return false;
-        } else if (!password.equals(other.password))
-            return false;
-        if (username == null) {
-            if (other.username != null)
-                return false;
-        } else if (!username.equals(other.username))
-            return false;
-        return true;
-    }
-    @Override
     public String toString() {
         return this.username + "-->" + this.password + "-->" + this.age;
     }
