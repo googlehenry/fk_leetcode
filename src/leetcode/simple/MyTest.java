8ffba3f74d43a8a25ce4974d4d449fd5663e6df0
a5365958 <2571706435@qq.com>
2022-12-05 12:30:50 +0800

    409. 最长回文串

--- src/leetcode/simple/MyTest.java
+++ src/leetcode/simple/MyTest.java
@@ -8,30 +8,54 @@ import java.util.jar.JarEntry;
 public class MyTest {
 
     public static void main(String[] args) throws InterruptedException {
-        System.out.println(new MyTest().lengthOfLongestSubstring("tmmzuxt"));
+        System.out.println(new MyTest().longestPalindrome("abccccdd"));
     }
 
 
-    /**最长不重复子串
+    /**
+     * 409. 最长回文串
+     * 简单
+     * 489
+     * 相关企业
+     * 给定一个包含大写字母和小写字母的字符串 s ，返回 通过这些字母构造成的 最长的回文串 。
      *
+     * 在构造过程中，请注意 区分大小写 。比如 "Aa" 不能当做一个回文字符串。
+     *
+     *
+     *
+     * 示例 1:
+     *
+     * 输入:s = "abccccdd"
+     * 输出:7
+     * 解释:
+     * 我们可以构造的最长的回文串是"dccaccd", 它的长度是 7。
+     * 示例 2:
+     *
+     * 输入:s = "a"
+     * 输入:1
+     * 示例 3：
+     *
+     * 输入:s = "aaaaaccc"
+     * 输入:7
      * @param s
      * @return
      */
-    public int lengthOfLongestSubstring(String s) {
-        if (s.length() == 1) return 1;
-        String max = "";
-        Map<Character,Integer> idxMap = new HashMap<>();
-        for (int i = 0, j = 0; i < s.length()&&j<s.length(); j++) {
-            Integer prevIdx = idxMap.get(s.charAt(j));
-            if(prevIdx!=null){
-                i = Math.max(i,prevIdx+1);
-            }
-            if(max.length()<j-i+1){
-                max = s.substring(i,j+1);
-            }
-            idxMap.put(s.charAt(j),j);
+    public int longestPalindrome(String s) {
+        Map<Character,Integer> charCount = new HashMap<>();
+
+        for(int i=0;i<s.length();i++){
+            Character  character = s.charAt(i);
+            charCount.put(character,charCount.get(character)==null?1:charCount.get(character)+1);
         }
-        return max.length();
+
+        int sumOfEven = 0;
+        int sumOfOdd = 0;
+        for(int count:charCount.values()){
+            sumOfEven += (count/2)*2;
+            sumOfOdd += count%2;
+        }
+
+        return sumOfEven + (sumOfOdd>0?1:0);
     }
 }
 
